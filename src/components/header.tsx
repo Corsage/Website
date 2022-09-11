befaab64393961d4afb1ed695da802f8bda5cf28
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-11 17:21:39 -0400

    Base header created.

---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="flex h-full px-5">
+        <div className="my-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="#89D9C6"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
