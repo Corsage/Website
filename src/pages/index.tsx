a9be4a1b445440b5f8d58df273a83f231d631a8c
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-11 17:22:07 -0400

    Design changes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,20 @@ import type { HeadFC } from 'gatsby';
 
 import Layout from '../components/layout';
 
-const IndexPage = () => {
+const Home = () => {
   return (
-    <Layout>
-      <div className="flex m-auto">
-        <input
-          type="text"
-          name="search-query"
-          id="search-query"
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-        />
-      </div>
-    </Layout>
+    <div className="flex m-auto">
+      <input
+        style={{ height: 42, width: 720 }}
+        type="text"
+        name="search-query"
+        id="search-query"
+        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+      />
+    </div>
   );
 };
 
-export default IndexPage;
+export default Home;
 
 export const Head: HeadFC = () => <title>Home Page</title>;
