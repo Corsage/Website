7a81d156d2e9f30c88efec42ba67ed736b59c530
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-13 01:09:55 -0400

    Menu now using icon.

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import Icon from '../assets/svg/menu.svg';
+
 const Menu = () => {
   const [open, isOpen] = useState(false);
 
@@ -9,7 +11,7 @@ const Menu = () => {
         id="collapsable-menu"
         className={`fixed top-0 ${
           open ? 'left-0' : 'left-[-250px]'
-        } w-[240px] h-screen z-50 bg-gray-700 p-5
+        } w-[240px] h-screen z-50 bg-teal-900 p-5
     flex flex-col space-y-5 text-white duration-300`}
       >
         <a
@@ -33,21 +35,8 @@ const Menu = () => {
         </a>
       </div>
 
-      <span className="cursor-pointer text-4xl" onClick={() => isOpen(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="#89D9C6"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
+      <span className="cursor-pointer" onClick={() => isOpen(true)}>
+        <Icon className="icon" />
       </span>
     </div>
   );
