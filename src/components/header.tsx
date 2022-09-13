c4b0ec29e8c1081d959c9642100413cb8c27e329
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-13 01:09:28 -0400

    Updated header items.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,59 @@ import React from 'react';
 
 import Menu from './menu';
 
+import Gmail from '../assets/svg/gmail.svg';
+import Github from '../assets/svg/github.svg';
+import LinkedIn from '../assets/svg/linkedin.svg';
+
 const Header = () => {
   return (
-    <div className="header">
-      <div className="flex h-full px-5">
-        <div className="my-auto">
+    <nav
+      style={styles.header}
+      className="navbar py-2 relative flex items-center w-full justify-between"
+    >
+      <div className="px-6 w-full flex flex-wrap items-center justify-between">
+        <div className="flex items-center w-full">
           <Menu />
+
+          <ul className="navbar-nav flex ml-auto ">
+            <li className="nav-item">
+              <a
+                href="mailto:jay.corsage@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Gmail className="icon mr-5" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://github.com/Corsage"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github className="icon mr-5" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="https://linkedin.com/in/corsage/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedIn className="icon" />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
+    </nav>
   );
+};
+
+const styles = {
+  header: {
+    minHeight: '60px'
+  }
 };
 
 export default Header;
