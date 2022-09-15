4bc4485d2f8456f8bcf012f9ac97190b9ab15b7a
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-15 01:26:40 -0400

    Updated siteUrl.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Corsage`,
-    siteUrl: `https://www.corsage.rebirth.dev`
+    siteUrl: `https://corsage.rebirth.dev`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
