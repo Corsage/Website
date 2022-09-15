860444d77c7a9524bc42d9871c48df05212df802
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-14 20:05:04 -0400

    Fixed waves.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,16 @@
-import * as React from 'react';
+import React from 'react';
 import { Canvas, RootState, useFrame } from '@react-three/fiber';
 import { setUncaughtExceptionCaptureCallback } from 'process';
 
 const Footer = () => {
   const mesh = React.useRef();
-  const geometry = React.useRef();
+  // const geometry = React.useRef();
+
   const [hovered, setHover] = React.useState(false);
+
+  let zValues: number[] = [];
+  let count: number = 0;
+
   /** Setup plane waves. */
   const setup = {
     vertexHeight: 15000,
@@ -30,52 +35,46 @@ const Footer = () => {
   };
 
   const Scene = () => {
-    const [count, setCount] = React.useState(0.0);
-    const [vertices, setVertices] = React.useState([]);
+    const updateBufferGeometry = (time: number) => {
+      const geometry = mesh.current.geometry;
+      const position = geometry.attributes.position;
 
-    const updateBufferGeometry = () => {
-      const positions = geometry.current.attributes.position.array;
-      // planeGeo.vertices[i].z = Math.sin(( i + count * 0.00002)) * (planeGeo.vertices[i]._myZ - (planeGeo.vertices[i]._myZ* 0.6))
-
-      /*
-      for (let i = 0; i < positions.length; i += 3) {
-        positions[i + 2] =
-          Math.sin(i + count * 0.00002) *
-          (vertices[i + 2] - vertices[i + 2] * 0.6);
-        setCount(count + 0.1);
+      if (time === 0) {
+        // Initial setup - we assign a random height to the z-axis
+        // of each vertex.
+        for (let i = 0; i < position.count; i++) {
+          const height =
+            Math.random() * setup.vertexHeight - setup.vertexHeight;
+          position.array[i * 3 + 2] += height;
+          zValues.push(position.array[i * 3 + 2]);
+        }
+      } else {
+        for (let i = 0; i < position.count; i++) {
+          position.array[i * 3 + 2] =
+            Math.sin(i + count * 0.00002) * (zValues[i] - zValues[i] * 0.6);
+          count += 0.1;
+        }
       }
 
-      geometry.current.verticesNeedUpate = true;
-      */
+      position.needsUpdate = true;
     };
 
     useFrame((state: RootState, delta: number, frame?: any) => {
       const camera = state.camera;
+      const time = state.clock.getElapsedTime();
+
       const x = camera.position.x;
       const z = camera.position.z;
 
-      camera.position.x = x * Math.cos(0.001) + z * Math.sin(0.001) - 10;
-      camera.position.z = z * Math.cos(0.001) - x * Math.sin(0.001) - 10;
+      // Rotating the camera slowly in a circular fashion.
+      camera.position.x = x * Math.cos(0.001) + z * Math.sin(0.001);
+      camera.position.z = z * Math.cos(0.001) - x * Math.sin(0.001);
 
-      camera.lookAt(0, 8000, 0);
+      // View from above.
+      camera.lookAt(0, 7000, 0);
 
-      updateBufferGeometry();
-    });
-
-    React.useEffect(() => {
-      // Setup vertices for the first time.
-      const positions = geometry.current.attributes.position.array;
-
-      for (let i = 0; i < positions.length; i += 3) {
-        positions[i + 2] =
-          Math.random() * setup.vertexHeight - setup.vertexHeight;
-      }
-
-      // Clone it for use.
-      // setVertices(positions);
-
-      geometry.current.verticesNeedUpate = true;
-      console.log('worked!');
+      // Update the vertices based on time passed.
+      updateBufferGeometry(time);
     });
 
     return (
@@ -88,7 +87,6 @@ const Footer = () => {
           onPointerOut={(e) => setHover(false)}
         >
           <planeBufferGeometry
-            ref={geometry}
             attach="geometry"
             args={[
               setup.planeSize,
