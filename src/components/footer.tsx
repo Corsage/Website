8719d9210cce713b04e9639c24f866858f2639b5
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-17 21:42:52 -0400

    Matched footer and header spacing.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,123 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { Canvas, RootState, useFrame } from '@react-three/fiber';
-import { setUncaughtExceptionCaptureCallback } from 'process';
+import React from 'react';
+import { Link } from 'gatsby';
 
 const Footer = () => {
-  const mesh = React.useRef();
-
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
-
-  const [hovered, setHover] = React.useState(false);
-
-  let zValues: number[] = [];
-  let count: number = 0;
-
-  /** Setup plane waves. */
-  const setup = {
-    vertexHeight: 15000,
-    planeSize: 2500000,
-    planeDefinition: 100,
-    background: '#1D594B',
-    meshColor: '#89D9C6'
-  };
-
-  const camera = {
-    fov: 50,
-    aspect: window.innerWidth / window.innerHeight,
-    near: 1,
-    far: 400000,
-    position: [0, 10000, 10000]
-  };
-
-  const test = ({ gl }: RootState) => {
-    gl.alpha = false;
-    gl.setSize(window.innerWidth, window.innerHeight);
-    gl.setClearColor(setup.background, 0);
-  };
-
-  const resizeWindow = () => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-  };
-
-  const Scene = () => {
-    const updateBufferGeometry = (time: number) => {
-      const geometry = mesh.current.geometry;
-      const position = geometry.attributes.position;
-
-      if (time === 0) {
-        // Initial setup - we assign a random height to the z-axis
-        // of each vertex.
-        for (let i = 0; i < position.count; i++) {
-          const height =
-            Math.random() * setup.vertexHeight - setup.vertexHeight;
-          position.array[i * 3 + 2] += height;
-          zValues.push(position.array[i * 3 + 2]);
-        }
-      } else {
-        for (let i = 0; i < position.count; i++) {
-          position.array[i * 3 + 2] =
-            Math.sin(i + count * 0.00002) * (zValues[i] - zValues[i] * 0.6);
-          count += 0.1;
-        }
-      }
-
-      position.needsUpdate = true;
-    };
-
-    useFrame((state: RootState, delta: number, frame?: any) => {
-      const camera = state.camera;
-      const time = state.clock.getElapsedTime();
-
-      const x = camera.position.x;
-      const z = camera.position.z;
-
-      // Rotating the camera slowly in a circular fashion.
-      camera.position.x = x * Math.cos(0.001) + z * Math.sin(0.001);
-      camera.position.z = z * Math.cos(0.001) - x * Math.sin(0.001);
-
-      // View from above.
-      camera.lookAt(0, 7000, 0);
-
-      // Update the vertices based on time passed.
-      updateBufferGeometry(time);
-    });
-
-    return (
-      <>
-        <fog attach="fog" color={setup.background} near={1} far={300000} />
-        <mesh
-          ref={mesh}
-          rotation-x={Math.PI * -0.5}
-          onPointerOver={(e) => setHover(true)}
-          onPointerOut={(e) => setHover(false)}
-        >
-          <planeBufferGeometry
-            attach="geometry"
-            args={[
-              setup.planeSize,
-              setup.planeSize,
-              setup.planeDefinition,
-              setup.planeDefinition
-            ]}
-          />
-          <meshBasicMaterial
-            attach="material"
-            color={setup.meshColor}
-            wireframe
-          />
-        </mesh>
-      </>
-    );
-  };
-
   return (
-    <div className="footer">
-      <Canvas camera={camera} onCreated={test}>
-        <Scene />
-      </Canvas>
-    </div>
+    <footer className="navbar py-6 relative flex items-center w-full justify-between">
+      <div className="px-6 w-full flex flex-wrap items-center justify-between">
+        <div className="flex flex-col-reverse md:flex-row items-center w-full">
+          <span className="text-sm text-green-cyan sm:text-center">
+            &copy; {new Date().getFullYear().toString()} Corsage. All Rights
+            Reserved.
+          </span>
+
+          <ul className="navbar-nav flex md:ml-auto mb-1 md:mb-0">
+            <li className="nav-item mr-5">
+              <Link
+                to="/"
+                className="text-sm text-green-cyan hover:text-light-green-cyan"
+                activeClassName="text-light-green-cyan"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item mr-5">
+              <Link
+                to="/portfolio"
+                className="text-sm text-green-cyan hover:text-light-green-cyan"
+                activeClassName="text-light-green-cyan"
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item mr-5">
+              <Link
+                to="/projects"
+                className="text-sm text-green-cyan hover:text-light-green-cyan"
+                activeClassName="text-light-green-cyan"
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="text-sm text-green-cyan hover:text-light-green-cyan"
+                activeClassName="text-light-green-cyan"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   );
 };
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,31 +9,28 @@ import LinkedIn from '../assets/svg/linkedin.svg';
 
 const Header = () => {
   return (
-    <nav
-      style={styles.header}
-      className="navbar py-2 relative flex items-center w-full justify-between"
-    >
+    <nav className="navbar py-6 relative flex items-center w-full justify-between">
       <div className="px-6 w-full flex flex-wrap items-center justify-between">
         <div className="flex items-center w-full">
           <Menu />
 
-          <ul className="navbar-nav flex ml-auto ">
-            <li className="nav-item">
+          <ul className="navbar-nav flex ml-auto">
+            <li className="nav-item mr-5">
               <OutboundLink
                 href="mailto:jay.corsage@gmail.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Gmail className="icon mr-5 text-green-cyan hover:text-light-green-cyan" />
+                <Gmail className="icon text-green-cyan hover:text-light-green-cyan" />
               </OutboundLink>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mr-5">
               <OutboundLink
                 href="https://github.com/Corsage"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Github className="icon mr-5 text-green-cyan hover:text-light-green-cyan" />
+                <Github className="icon text-green-cyan hover:text-light-green-cyan" />
               </OutboundLink>
             </li>
             <li className="nav-item">
@@ -50,12 +47,6 @@ const Header = () => {
       </div>
     </nav>
   );
-};
-
-const styles = {
-  header: {
-    minHeight: '60px'
-  }
 };
 
 export default Header;
