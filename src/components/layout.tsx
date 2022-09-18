c156229df766d7e767a173ac9aa07fc9c4931e9c
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-17 21:43:14 -0400

    Waves now at bottom of all pages.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,18 +3,30 @@ import React, { ReactNode } from 'react';
 import Footer from './footer';
 import Header from './header';
 
+import Waves from './waves';
+
 interface Props {
   children?: ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="relative flex flex-col min-h-screen">
       <Header />
       <main className="container flex flex-1 mx-auto">{children}</main>
       <Footer />
+
+      <div style={styles.wavesContainer} className="absolute bottom-0 -z-50">
+        <Waves />
+      </div>
     </div>
   );
+};
+
+const styles = {
+  wavesContainer: {
+    height: 400
+  }
 };
 
 export default Layout;
