2e718ec7ae749ce9e0e8e13be4b75100d7d73d57
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-09-27 23:45:53 -0400

    Header now controls menu state.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { OutboundLink } from 'gatsby-plugin-google-gtag';
 
 import Menu from './menu';
@@ -8,11 +8,36 @@ import Github from '../assets/svg/github.svg';
 import LinkedIn from '../assets/svg/linkedin.svg';
 
 const Header = () => {
+  const [sideBarOpen, setSideBarOpen] = useState(false);
+
   return (
     <nav className="navbar py-6 relative flex items-center w-full justify-between">
+      <Menu isOpen={sideBarOpen} setOpen={setSideBarOpen} />
+
       <div className="px-6 w-full flex flex-wrap items-center justify-between">
         <div className="flex items-center w-full">
-          <Menu />
+          <button
+            className="icon flex items-center justify-center relative text-cyan hover:text-accent"
+            onClick={() => setSideBarOpen(true)}
+          >
+            <span
+              className={`transform transition w-full h-px bg-current absolute ${
+                sideBarOpen ? 'translate-y-0 rotate-45' : '-translate-y-2'
+              }`}
+            />
+
+            <span
+              className={`transform transition w-full h-px bg-current absolute ${
+                sideBarOpen ? 'opacity-0 translate-x-3' : 'opacity-100'
+              }`}
+            />
+
+            <span
+              className={`transform transition w-full h-px bg-current absolute ${
+                sideBarOpen ? 'translate-y-0 -rotate-45' : 'translate-y-2'
+              }`}
+            />
+          </button>
 
           <ul className="navbar-nav flex ml-auto">
             <li className="nav-item mr-5">
