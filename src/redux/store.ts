3286542b78d03e204658ac57b0615dc3583efca7
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-10-03 16:49:44 -0400

    Now using redux-persist.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { enableMapSet } from 'immer';
 
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import globalReducer from './slices/globalSlice';
 import notificationReducer from './slices/notificationSlice';
 
 enableMapSet();
 
-const store = configureStore({
-  reducer: {
-    notification: notificationReducer
-  }
+const rootReducer = combineReducers({
+  global: globalReducer,
+  notification: notificationReducer
 });
 
-export default store;
+const persistedReducer = persistReducer(
+  {
+    key: 'root',
+    storage: storage,
+    whitelist: ['global']
+  },
+  rootReducer
+);
+
+export const store = configureStore({
+  reducer: persistedReducer
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
