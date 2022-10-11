feaaafcd01eb90500a558aa526da099b0ccd5a13
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-10-11 01:07:01 -0400

    Revamped waves.

--- src/components/waves.tsx
+++ src/components/waves.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Canvas, RootState, useFrame } from '@react-three/fiber';
+import { Camera, Mesh, Vector3 } from 'three';
 
+/**
+ * Dynamically creates waves using Three.js and react-three/fiber.
+ *
+ * @remarks
+ * Inspired by a waves concept I once saw on codepden.
+ *
+ * @returns A Three.js canvas with a custom camera and GL.
+ *
+ * @beta
+ */
 const Waves = () => {
-  const mesh = useRef();
-
-  const [hovered, setHover] = useState(false);
-
-  let zValues: number[] = [];
-  let count: number = 0;
-
   /** Setup plane waves. */
   const setup = {
     vertexHeight: 15000,
@@ -18,39 +22,60 @@ const Waves = () => {
     meshColor: '#89D9C6'
   };
 
+  /** Setup camera. */
   const camera = {
     fov: 50,
     aspect: window.innerWidth / window.innerHeight,
     near: 1,
     far: 400000,
-    position: [0, 10000, 10000]
+    position: new Vector3(0, 10000, 10000)
   };
 
-  const test = ({ gl }: RootState) => {
-    gl.alpha = false;
+  /** Custom GL settings. */
+  const setupGL = ({ gl }: RootState) => {
     gl.setSize(window.innerWidth, window.innerHeight);
     gl.setClearColor(setup.background, 0);
   };
 
+  /**
+   * Generates the waves motions (up and down).
+   *
+   * @param zValues - The initial z values of each pos vector.
+   * @param count - Keeps track of frame for use in sin func.
+   *
+   * @returns A Three.js fog and mesh for the waves.
+   *
+   * @beta
+   */
   const Scene = () => {
-    const updateBufferGeometry = (time: number) => {
+    const mesh = useRef<Mesh>(null!);
+
+    let zValues: number[] = [];
+    let count: number = 0;
+
+    const updateBufferGeometry = () => {
       const geometry = mesh.current.geometry;
       const position = geometry.attributes.position;
 
-      if (time === 0) {
+      if (zValues.length === 0) {
         // Initial setup - we assign a random height to the z-axis
         // of each vertex.
+
         for (let i = 0; i < position.count; i++) {
           const height =
-            Math.random() * setup.vertexHeight - setup.vertexHeight;
-          position.array[i * 3 + 2] += height;
-          zValues.push(position.array[i * 3 + 2]);
+            position.getZ(i) +
+            (Math.random() * setup.vertexHeight - setup.vertexHeight);
+
+          position.setZ(i, height);
+          zValues.push(height);
         }
       } else {
         for (let i = 0; i < position.count; i++) {
-          position.array[i * 3 + 2] =
+          const height =
             Math.sin(i + count * 0.00002) * (zValues[i] - zValues[i] * 0.6);
           count += 0.1;
+
+          position.setZ(i, height);
         }
       }
 
@@ -72,19 +97,14 @@ const Waves = () => {
       camera.lookAt(0, 7000, 0);
 
       // Update the vertices based on time passed.
-      updateBufferGeometry(time);
+      updateBufferGeometry();
     });
 
     return (
       <>
         <fog attach="fog" color={setup.background} near={1} far={300000} />
-        <mesh
-          ref={mesh}
-          rotation-x={Math.PI * -0.5}
-          onPointerOver={(e) => setHover(true)}
-          onPointerOut={(e) => setHover(false)}
-        >
-          <planeBufferGeometry
+        <mesh ref={mesh} rotation-x={Math.PI * -0.5}>
+          <planeGeometry
             attach="geometry"
             args={[
               setup.planeSize,
@@ -93,18 +113,14 @@ const Waves = () => {
               setup.planeDefinition
             ]}
           />
-          <meshBasicMaterial
-            attach="material"
-            color={setup.meshColor}
-            wireframe
-          />
+          <meshBasicMaterial attach="material" color={setup.meshColor} />
         </mesh>
       </>
     );
   };
 
   return (
-    <Canvas camera={camera} onCreated={test}>
+    <Canvas camera={camera} onCreated={setupGL}>
       <Scene />
     </Canvas>
   );
