c4ee907df90afecadab0d99c3d0da7046a0e026d
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-10-14 15:05:23 -0400

    Added pages for tags.

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,14 @@
 import type { GatsbyNode } from 'gatsby';
-import { createFilePath } from 'gatsby-source-filesystem';
 import path from 'path';
+
+const toKebabCase = (s: string) => {
+  return s
+    .match(
+      /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+    )!
+    .join('-')
+    .toLowerCase();
+};
 
 export const createPages: GatsbyNode['createPages'] = async ({
   graphql,
@@ -18,6 +26,11 @@ export const createPages: GatsbyNode['createPages'] = async ({
           id
         }
       }
+      tags: allMarkdownRemark {
+        group(field: frontmatter___tags) {
+          fieldValue
+        }
+      }
     }
   `);
 
@@ -30,13 +43,26 @@ export const createPages: GatsbyNode['createPages'] = async ({
   }
 
   const posts = res.data.posts.nodes;
+  const tags = res.data.tags.group;
 
-  posts.forEach((node) => {
+  // Create a page dynamically for each blog post, i.e. markdown file.
+  posts.forEach((node: any) => {
     createPage({
       path: `blog/${node.id}`,
-      component: path.resolve('src/components/blog/post-page.tsx'),
+      component: path.resolve('src/templates/post-page.tsx'),
       context: {
         id: node.id
+      }
+    });
+  });
+
+  // Create a page for each tag.
+  tags.forEach((tag: any) => {
+    createPage({
+      path: `blog/tags/${toKebabCase(tag.fieldValue)}`,
+      component: path.resolve('./src/templates/tag-page.tsx'),
+      context: {
+        tag: tag.fieldValue
       }
     });
   });
