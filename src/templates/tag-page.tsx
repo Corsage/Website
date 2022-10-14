46dce77ceaef5238efa9eaba62637e73e5bbefd8
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-10-14 15:06:27 -0400

    Added page for tags.

---
+++ src/templates/tag-page.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { graphql, StaticQueryDocument } from 'gatsby';
+import PostItem from '../components/blog/post-item';
+import BlogPost from '../models/blog-post';
+
+interface Props {
+  pageContext: { tag: string };
+  data: StaticQueryDocument;
+}
+
+const TagPage = ({ pageContext, data }: Props) => {
+  const { tag } = pageContext;
+  console.log(tag);
+  console.log(JSON.stringify(data));
+
+  const { posts } = data;
+
+  return (
+    <div className="w-full my-10">
+      <div className="text-center text-white my-10">
+        <h1 className="text-4xl font-semibold uppercase tracking-widest">
+          {posts.totalCount} post(s) tagged "{tag}"
+        </h1>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start gap-6 my-10">
+        {posts.nodes.map((node: any) => {
+          const post: BlogPost = {
+            id: node.id,
+            title: node.frontmatter.title,
+            tags: node.frontmatter.tags,
+            date: node.frontmatter.date,
+            excerpt: node.excerpt,
+            timeToRead: node.timeToRead
+          };
+
+          return <PostItem key={`blog-post-${post.id}`} post={post} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query ($tag: String!) {
+    posts: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      totalCount
+      nodes {
+        id
+        excerpt
+        timeToRead
+        frontmatter {
+          title
+          tags
+          date(formatString: "MMMM DD, YYYY")
+          description
+        }
+      }
+    }
+  }
+`;
+
+export default TagPage;
