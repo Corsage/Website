ec7b4f6c0d3761a89cbe07115f5b75dec6f87bc6
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-10-15 15:18:24 -0400

    Now using dark-mode.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,17 +6,17 @@ const Footer = () => {
     <footer className="navbar py-6 relative flex items-center w-full justify-between">
       <div className="px-6 w-full flex flex-wrap items-center justify-between">
         <div className="flex flex-col-reverse md:flex-row items-center w-full">
-          <span className="text-sm text-cyan sm:text-center">
+          <span className="text-sm sm:text-center text-cyan dark:text-medium-cyan">
             &copy; {new Date().getFullYear().toString()} Corsage. All Rights
             Reserved.
           </span>
 
-          <ul className="navbar-nav flex text-cyan md:ml-auto mb-1 md:mb-0">
+          <ul className="navbar-nav flex md:ml-auto mb-1 md:mb-0 text-cyan dark:text-medium-cyan">
             <li className="nav-item mr-5">
               <Link
                 to="/"
-                className="text-sm hover:text-accent"
-                activeClassName="text-accent"
+                className="text-sm hover:text-accent dark:hover:text-dark-accent"
+                activeClassName="text-accent dark:text-dark-accent"
               >
                 Home
               </Link>
@@ -25,8 +25,8 @@ const Footer = () => {
               <Link
                 to="/portfolio"
                 partiallyActive={true}
-                className="text-sm hover:text-accent"
-                activeClassName="text-accent"
+                className="text-sm hover:text-accent dark:hover:text-dark-accent"
+                activeClassName="text-accent dark:text-dark-accent"
               >
                 Portfolio
               </Link>
@@ -35,8 +35,8 @@ const Footer = () => {
               <Link
                 to="/blog"
                 partiallyActive={true}
-                className="text-sm hover:text-accent"
-                activeClassName="text-accent"
+                className="text-sm hover:text-accent dark:hover:text-dark-accent"
+                activeClassName="text-accent dark:text-dark-accent"
               >
                 Blog
               </Link>
@@ -45,8 +45,8 @@ const Footer = () => {
               <Link
                 to="/contact"
                 partiallyActive={true}
-                className="text-sm hover:text-accent"
-                activeClassName="text-accent"
+                className="text-sm hover:text-accent dark:hover:text-dark-accent"
+                activeClassName="text-accent dark:text-dark-accent"
               >
                 Contact
               </Link>
