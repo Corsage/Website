27e79579207293ddf3c71556e0557a9aed9592dc
Jay Chowdhary <jayc.chowdhary@gmail.com>
2022-10-15 15:18:35 -0400

    Now using dark-mode.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,7 +18,7 @@ const Layout = ({ children, location }: PageProps) => {
   );
 
   return (
-    <div className="relative flex flex-col min-h-screen">
+    <div className={`relative flex flex-col min-h-screen ${waves && 'dark'}`}>
       {banner && (
         <Banner
           text="This website is currently in pre-release and may contain bugs."
